7b40696669373a21c6dcde724f1b3b4d89d5e1d8
pys789 <pys789@126.com>
2020-04-25 10:11:21 +0800

    修改括号匹配

--- leetcode/src/cn/pys/list/Num20.java
+++ leetcode/src/cn/pys/list/Num20.java
@@ -5,40 +5,40 @@ import java.util.Stack;
 
 public class Num20 {
 
-	public static void main(String[] args) {
-		System.out.println(isValid("()"));
-		System.out.println(isValid("(})"));
-		System.out.println(isValid("([])"));
-	}
+    public static void main(String[] args) {
+        System.out.println(isValid("()"));
+        System.out.println(isValid("(})"));
+        System.out.println(isValid("([])"));
+    }
 
-	private static HashMap<Character, Character> map = new HashMap<>();
-	static {
-		map.put('{', '}');
-		map.put('[', ']');
-		map.put('(', ')');
+    private static HashMap<Character, Character> map = new HashMap<>();
+    static {
+        map.put('{', '}');
+        map.put('[', ']');
+        map.put('(', ')');
 
-	}
+    }
 
-	public static boolean isValid(String s) {
+    private static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        if (s == null || "".equals(s))
+            return true;
+        char[] charArray = s.toCharArray();
+        for (char c : charArray) {
+            if (map.containsKey(c)) {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()){
+                    return false;
+                }
+                Character value = map.get(stack.pop());
+                if (c != value) {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
 
-		Stack<Character> stack = new Stack<>();
-		if (s == null || "".equals(s))
-			return true;
-		char[] charArray = s.toCharArray();
-		for (char c : charArray) {
-			if (map.containsKey(c)) {
-				stack.push(c);
-			} else {
-				if (stack.isEmpty())
-					return false;
-				Character value = map.get(stack.pop());
-				if (c != value) {
-					return false;
-				}
-			}
-		}
-		return stack.isEmpty();
-
-	}
+    }
 
 }
